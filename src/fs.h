6aff110aa4dcd6e0048d3bf0e8636500de31b452
Brandon Kammerdiener <kammerdienerb@gmail.com>
2019-11-12 10:49:37 -0800

    enable syntax_c for C++

--- src/fs.h
+++ src/fs.h
@@ -3,8 +3,9 @@
 #define FT_UNKNOWN (0)
 #define FT_TXT     (1)
 #define FT_C       (2)
-#define FT_SH      (3)
-#define FT_LATEX   (4)
+#define FT_CXX     (3)
+#define FT_SH      (4)
+#define FT_LATEX   (5)
 
 typedef struct {
     int ft;
