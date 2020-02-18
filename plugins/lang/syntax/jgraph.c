4ba787375e002d024ad6901dd7f9f1f64f0b5c73
Brandon Kammerdiener <kammerdienerb@gmail.com>
2020-02-18 13:45:11 -0500

    much better jgraph syntax highlighting

--- plugins/lang/syntax/jgraph.c
+++ plugins/lang/syntax/jgraph.c
@@ -3,7 +3,9 @@
 
 #define ARRAY_LOOP(a) for (__typeof((a)[0]) *it = (a); it < (a) + (sizeof(a) / sizeof((a)[0])); ++it)
 
-highlight_info hinfo;
+highlight_info hinfo1;
+highlight_info hinfo2;
+highlight_info hinfo3;
 
 void unload(yed_plugin *self);
 void syntax_jgraph_line_handler(yed_event *event);
@@ -14,16 +16,43 @@ void syntax_jgraph_buff_mod_post_handler(yed_event *event);
 
 int yed_plugin_boot(yed_plugin *self) {
     yed_event_handler frame, line, buff_mod_pre, buff_mod_post;
-    char              *kwds[] = {
-        "x",        "y",
-        "box",      "eps",      "pts",
-        "none",     "text",     "xbar",     "ybar",
-        "cross",    "solid",    "xaxis",    "yaxis",
-        "circle",   "dashed",   "dotted",
-        "diamond",  "dotdash",  "ellipse",  "general",
-        "longdash", "marktype", "newcurve", "newgraph", "triangle",
-        "dotdotdash", "postscript",
-        "dotdotdashdash",
+    char              *edit_kwds[] = {
+        "x", "y", "fontsize", "linesep",
+        "hjl", "hjc", "hjr", "vjc", "vjt", "vjb",
+        "font", "rotate", "lgray", "lcolor", "y_epts",
+        "pts", "x_epts", "label", "marksize", "gmarks",
+        "pfill", "pcfill", "fill", "cfill", "afill", "acfill",
+        "marktype", "glines", "pattern", "apattern", "ppattern",
+        "linetype", "linethickness", "gray", "color", "mrotate",
+        "poly", "nopoly", "nopoly", "bezier",
+        "nobezier", "asize", "clip", "noclip", "at", "size", "max",
+        "min", "hash", "shash", "mhash", "precision", "label",
+        "hash_format", "hash_labels", "log_base", "draw_at", "nodraw", "draw", "hash_at", "mhash_at", "hash_label",
+        "hash_scale", "auto_hash_marks", "no_auto_hash_marks",
+        "auto_hash_labels", "no_auto_hash_labels", "draw_hash_labels_at",
+        "draw_hash_marks_at", "no_draw_hash_labels", "draw_hash_labels",
+        "no_draw_axis_line", "draw_axis_line", "no_draw_axis", "draw_axis",
+        "no_draw_hash_marks", "draw_hash_marks", "no_draw_axis_label",
+        "draw_axis_label", "no_grid_lines", "grid_lines", "no_mgrid_lines",
+        "mgrid_lines", "grid_gray", "grid_color", "mgrid_gray",
+        "mgrid_color", "linelength", "columns", "columnsep", "linebreak",
+        "midspace", "defaults", "xaxis", "yaxis", "curve", "newcurve",
+        "copycurve", "newline", "title", "legend", "x_translate",
+        "y_translate", "string", "newstring", "copystring",
+        "copyline", "inherit_axes", "Y", "X", "border", "noborder", "clip",
+        "noclip", "graph", "newgraph", "copygraph", "bbox", "preample",
+        "epilogue",
+    };
+    char              *value_kwds[] = {
+        "none", "solid", "dotted", "dashed", "longdash", "dotdash",
+        "dotdotdash", "dotdotdashdash", "larrow",
+        "nolarrow", "rarrow", "norarrow", "larrows", "nolarrows",
+        "rarrows", "norarrows", "log", "linear",
+        "right", "left", "top", "bottom", "on", "off", "custom",
+        "circle", "box", "diamond", "triangle", "x", "cross",
+        "ellipse", "general", "general_nf", "general_bez",
+        "general_bez_nf", "postscript", "eps",
+        "xbar", "ybar", "text", "stripe", "estripe"
     };
 
     yed_plugin_set_unload_fn(self, unload);
@@ -43,15 +72,27 @@ int yed_plugin_boot(yed_plugin *self) {
     yed_plugin_add_event_handler(self, buff_mod_pre);
     yed_plugin_add_event_handler(self, buff_mod_post);
 
+    highlight_info_make(&hinfo1);
 
-    highlight_info_make(&hinfo);
+    highlight_add_kwd(&hinfo1, "shell", HL_KEY);
 
-    ARRAY_LOOP(kwds)
-        highlight_add_kwd(&hinfo, *it, HL_KEY);
-    highlight_numbers(&hinfo);
-    highlight_within(&hinfo, "\"", "\"", '\\', -1, HL_STR);
-    highlight_within(&hinfo, "'", "'", '\\', 1, HL_CHAR);
-    highlight_within_multiline(&hinfo, "(*", "*)", 0, HL_COMMENT);
+
+    highlight_info_make(&hinfo2);
+
+    ARRAY_LOOP(edit_kwds)
+        highlight_add_kwd(&hinfo2, *it, HL_KEY);
+    ARRAY_LOOP(value_kwds)
+        highlight_add_kwd(&hinfo2, *it, HL_CON);
+    highlight_add_kwd(&hinfo2, "include", HL_PP);
+    highlight_to_eol_from(&hinfo2, ":", HL_CALL);
+    highlight_to_eol_from(&hinfo2, "shell", HL_IGNORE);
+    highlight_numbers(&hinfo2);
+
+    highlight_info_make(&hinfo3);
+
+    highlight_within_multiline(&hinfo3, "\"", "\"", '\\', HL_STR);
+    highlight_within_multiline(&hinfo3, "'", "'", '\\', HL_CHAR);
+    highlight_within_multiline(&hinfo3, "(*", "*)", 0, HL_COMMENT);
 
     ys->redraw = 1;
 
@@ -59,7 +100,8 @@ int yed_plugin_boot(yed_plugin *self) {
 }
 
 void unload(yed_plugin *self) {
-    highlight_info_free(&hinfo);
+    highlight_info_free(&hinfo2);
+    highlight_info_free(&hinfo1);
     ys->redraw = 1;
 }
 
@@ -75,7 +117,7 @@ void syntax_jgraph_frame_handler(yed_event *event) {
         return;
     }
 
-    highlight_frame_pre_draw_update(&hinfo, event);
+    highlight_frame_pre_draw_update(&hinfo2, event);
 }
 
 void syntax_jgraph_line_handler(yed_event *event) {
@@ -90,7 +132,9 @@ void syntax_jgraph_line_handler(yed_event *event) {
         return;
     }
 
-    highlight_line(&hinfo, event);
+    highlight_line(&hinfo1, event);
+    highlight_line(&hinfo2, event);
+    highlight_line(&hinfo3, event);
 }
 
 void syntax_jgraph_buff_mod_pre_handler(yed_event *event) {
@@ -105,7 +149,7 @@ void syntax_jgraph_buff_mod_pre_handler(yed_event *event) {
         return;
     }
 
-    highlight_buffer_pre_mod_update(&hinfo, event);
+    highlight_buffer_pre_mod_update(&hinfo3, event);
 }
 
 void syntax_jgraph_buff_mod_post_handler(yed_event *event) {
@@ -120,5 +164,5 @@ void syntax_jgraph_buff_mod_post_handler(yed_event *event) {
         return;
     }
 
-    highlight_buffer_post_mod_update(&hinfo, event);
+    highlight_buffer_post_mod_update(&hinfo3, event);
 }
