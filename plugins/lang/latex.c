cedfcbab921d430d57f60bfd66075f4340f45d79
Brandon Kammerdiener <kammerdienerb@gmail.com>
2020-02-27 15:52:13 -0500

    make lang plugins unload subplugins on unload

--- plugins/lang/latex.c
+++ plugins/lang/latex.c
@@ -1,8 +1,17 @@
 #include <yed/plugin.h>
 
+void unload(yed_plugin *self);
+
 int yed_plugin_boot(yed_plugin *self) {
+    yed_plugin_set_unload_fn(self, unload);
+
     YEXE("plugin-load", "lang/syntax/latex");
     YEXE("plugin-load", "lang/tools/latex");
 
     return 0;
+}
+
+void unload(yed_plugin *self) {
+    YEXE("plugin-unload", "lang/syntax/latex");
+    YEXE("plugin-unload", "lang/tools/latex");
 }
