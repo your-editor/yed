3ec956d3fffeff3628d12a56a6e8a64120626349
Brandon Kammerdiener <kammerdienerb@gmail.com>
2020-07-02 13:48:17 -0400

    expose yed_frame_line_to_y()

--- src/frame.h
+++ src/frame.h
@@ -61,4 +61,8 @@ void frame_get_rect(yed_frame *frame, int *top,  int *left,  int *height,  int *
 #define FRAME_RESET_RECT(f) frame_get_rect((f), &(f)->top,  &(f)->left,  &(f)->height,  &(f)->width,\
                                                 &(f)->btop, &(f)->bleft, &(f)->bheight, &(f)->bwidth)
 
+int yed_cell_is_in_frame(int row, int col, yed_frame *frame);
+
+int yed_frame_line_to_y(yed_frame *frame, int row);
+
 #endif
