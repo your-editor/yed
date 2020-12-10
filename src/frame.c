938842f42a1e52e84eb4473a2ccf2796a39d376f
Brandon Kammerdiener <kammerdienerb@gmail.com>
2020-12-09 22:39:23 -0500

    fix crash when activating a frame whose buffer has shrunken smaller than where the cursor use to be

--- src/frame.c
+++ src/frame.c
@@ -212,6 +212,8 @@ yed_frame * yed_hsplit_frame(yed_frame *frame) {
 }
 
 void yed_activate_frame(yed_frame *frame) {
+    int       buff_n_lines;
+    int       save_cursor_line;
     yed_event event;
 
     event.kind  = EVENT_FRAME_ACTIVATED;
@@ -228,9 +230,17 @@ void yed_activate_frame(yed_frame *frame) {
      * Correct the cursor if the buffer has changed
      * while this frame was inactive.
      */
-    if (frame->buffer && frame->buffer->kind == BUFF_KIND_YANK) {
-        yed_set_cursor_far_within_frame(frame, 1, 1);
+    if (frame->buffer) {
+        buff_n_lines = bucket_array_len(frame->buffer->lines);
+        if (frame->cursor_line > buff_n_lines) {
+            save_cursor_line = frame->cursor_line;
+            yed_set_cursor_far_within_frame(frame, 1, 1);
+            yed_set_cursor_far_within_frame(frame, 1, save_cursor_line);
+        } else if (frame->buffer->kind == BUFF_KIND_YANK) {
+            yed_set_cursor_far_within_frame(frame, 1, 1);
+        }
     }
+
     yed_frame_reset_cursor(frame);
     ys->active_frame->dirty = 1;
     ys->redraw              = 1;
