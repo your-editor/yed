f74868a43f34187bfc3b62a468153d6a4689d851
Brandon Kammerdiener <kammerdienerb@MacBook-Pro-2.local>
2021-11-30 13:37:45 -0500

    syntax.h

--- src/syntax.h
+++ src/syntax.h
@@ -1,40 +1,124 @@
+/*
+ * === What this file provides:
+ *
+ * Fast highlighting of ASCII keywords matching [a-Z_]+[0-9a-Z_]
+ *
+ *     Keywords are stored in length buckets so that if we know the length
+ *     of the next word in the line, we can search for matches only in one
+ *     bucket and cap the string comparisons at word_lenth characters.
+ *
+ *     The main advantage of this is that most keywords are somewhere in the
+ *     range of 2-8 characters for most languages, but other words that may
+ *     appear in the buffer are typically longer (like identifiers).
+ *     In that case, we don't have to search for a match for a 16 character
+ *     word because we won't have a 16 character keyword bucket.
+ *
+ *     The other optimization that can be made for simple keywords is that
+ *     we can highlight them all in a single traversal of the line/string.
+ *
+ * Matching of regular expressions on a single line
+ *     Submatches can be specified.
+ *
+ * Single/multi-line ranges defined by start/end regular expressions.
+ *     Can include regular expression ranges to skip (e.g. skip \" in a string literal).
+ *     Regular expressions and keywords can be specified for highlighting within a specific range.
+ *
+ * A smart and fast caching strategy that stores states of lines with respect to multi-line range state.
+ *     Self-balances the distribution of cache entries so that buffers get good coverage and random access
+ *     patterns don't cause huge slowdown.
+ *     Always keeps the most recently drawn line in cache so that scrolling and full frame redraws are very quick.
+ *     Must parse whole buffer at least once so that the cache can always be correct (if highlighting is to be correct
+ *     100% of the time, this is an unfortunate necessity). :(
+ *
+ * A declarative interface for defining syntax.
+ *
+ *
+ * === How to use this file:
+ *
+ *     #include <yed/syntax.h>
+ *
+ * Create a yed_syntax structure:
+ *
+ *     static yed_syntax syn;
+ *
+ * Use the declarative interface to build up the syntax (this example doesn't check for errors):
+ *
+ *     yed_syntax_start(&syn);
+ *
+ *         // Highlight string literals with escape sequences.
+ *         yed_syntax_attr_push(&syn, "&code-string");
+ *             yed_syntax_range_start(&syn, "\"");
+ *                 yed_syntax_range_one_line(&syn);
+ *                 yed_syntax_range_skip(&syn, "\\\\\"");
+ *
+ *                 yed_syntax_attr_push(&syn, "&code-escape-sequence");
+ *                     yed_syntax_regex(&syn, "\\\\.");
+ *                 yed_syntax_attr_pop();
+ *             yed_syntax_range_end(&syn, "\"");
+ *         yed_syntax_attr_pop(&syn);
+ *
+ *         // Highlight numbers.
+ *         yed_syntax_regex_sub(&syn, "(^|[^[:alnum:]_])(-?[[:digit:]]+)[[:>:]]", 2);
+ *
+ *         // Highlight the keyword "if".
+ *         yed_syntax_kwd(&syn, "if");
+ *
+ *     yed_syntax_end(&syn);
+ *
+ * Register event handlers for for these events that call the respective functions in this file:
+ *
+ *     event                     function call                           reason
+ *     ----------------------------------------------------------------------------------------------------------
+ *     EVENT_STYLE_CHANGE        yed_syntax_style_event(&syn);           Update attr structs for syntax elements.
+ *     EVENT_BUFFER_PRE_DELETE   yed_syntax_buffer_delete_event(&syn);   Remove state cache for the buffer.
+ *     EVENT_BUFFER_POST_MOD     yed_syntax_buffer_mod_event(&syn);      Update state cache for the buffer.
+ *     EVENT_LINE_PRE_DRAW       yed_syntax_line_event(&syn);            Highlight the line about to be drawn.
+ *
+ * You may choose to call yed_syntax_line_event() selectively if, for example, the buffer's ft matches the kind of
+ * buffer you're trying to highlight:
+ *
+ *     void eline(yed_event *event)  {
+ *         yed_frame *frame;
+ *
+ *         frame = event->frame;
+ *
+ *         if (!frame
+ *         ||  !frame->buffer
+ *         ||  frame->buffer->kind != BUFF_KIND_FILE
+ *         ||  frame->buffer->ft != yed_get_ft("C")) {
+ *             return;
+ *         }
+ *
+ *         yed_syntax_line_event(&syn, event);
+ *     }
+ *
+ * Free up the syntax structure when you're finished with it:
+ *
+ *     yed_syntax_free(&syn);
+ */
+
+
 #include <yed/internal.h>
 #include <yed/tree.h>
-
-#define is_space(c) (isspace(c))
-#define is_digit(c) (isdigit(c))
-#define is_alpha(c) (isalpha(c))
-#define is_alnum(c) (is_alpha(c) || is_digit(c))
-
 #include <regex.h>
 
 #define YED_SYN_CACHE_SIZE         (8192)
 #define YED_SYN_N_EVICTION_BUCKETS (4096)
 
+#ifdef YED_DEBUG
 
-#define DBG(...)          \
-do {                      \
-    LOG_FN_ENTER();       \
-    yed_log(__VA_ARGS__); \
-    LOG_EXIT();           \
+#define DBG(...)                      \
+do {                                  \
+    LOG_FN_ENTER();                   \
+    yed_log("syntax.h: "__VA_ARGS__); \
+    LOG_EXIT();                       \
 } while (0)
 
+#else
 
-/*
- * Fast highlighting of ASCII keywords matching [a-Z_]+[0-9a-Z_]
- * Keywords are stored in length buckets so that if we know the length
- * of the next word in the line, we can search for matches only in one
- * bucket and cap the string comparisons at word_lenth characters.
- *
- * The main advantage of this is that most keywords are somewhere in the
- * range of 2-8 characters for most languages, but other words that may
- * appear in the buffer are typically longer (like identifiers).
- * In that case, we don't have to search for a match for a 16 character
- * word because we won't have a 16 character keyword bucket.
- *
- * The other optimization that can be made for simple keywords is that
- * we can highlight them all in a single traversal of the line/string.
- */
+#define DBG(...) ;
+
+#endif
 
 typedef struct {
     char      *str;
@@ -269,6 +353,8 @@ static inline void _yed_syntax_free_range(_yed_syntax_range *range) {
     regfree(&range->start);
 
     _yed_syntax_free_items(&range->items);
+
+    free(range);
 }
 
 static inline void _yed_syntax_make_cache(_yed_syntax_cache *cache, u32 size) {
@@ -620,28 +706,29 @@ static _yed_syntax_range *_yed_syntax_get_start_state(yed_syntax *syntax, yed_bu
     return range;
 }
 
-static inline void _yed_syntax_fixup_cache(yed_syntax *syntax, yed_buffer *buffer, _yed_syntax_cache *cache, int row) {
+static inline int _yed_syntax_fixup_cache(yed_syntax *syntax, yed_buffer *buffer, _yed_syntax_cache *cache, int row) {
     _yed_syntax_cache_entry *it;
-    int                      count;
     _yed_syntax_range       *start_range;
     yed_line                *line;
     _yed_syntax_range       *end_range;
     int                      end_range_idx;
     _yed_syntax_cache_entry  new_entry;
+    int                      count;
+    int                      changed;
 
     if (array_len(cache->entries) == 0) {
-        return;
+        return 0;
     }
 
-    array_rtraverse(cache->entries, it) {
-        if (it->row <= row) { goto found; }
+    it = _yed_syntax_cache_lookup_nearest(syntax, cache, row);
+    if (it == NULL) {
+        it = array_data(cache->entries);
     }
 
-    it = array_data(cache->entries);
+    DBG("fixup: row %d ->", it->row);
 
-found:;
-
-    count = 0;
+    count   = 0;
+    changed = 0;
     while (it != array_last(cache->entries)) {
         start_range = *(_yed_syntax_range**)array_item(syntax->ranges, it->range_idx);
         line        = yed_buff_get_line(buffer, it->row);
@@ -658,12 +745,12 @@ found:;
             if ((it + 1)->range_idx == end_range_idx) {
                 /* The cache entry is correct, therefore all further cache entries are correct, so we're done! */
                 goto out;
-/*                 return; */
             }
 
             /* Update it. */
             it            = it + 1;
             it->range_idx = end_range_idx;
+            changed       = 1;
         } else {
             /* Insert a new entry. */
             it = _yed_syntax_add_to_cache(syntax, cache, it->row + 1, end_range);
@@ -674,21 +761,23 @@ found:;
     }
 
 out:;
-    DBG("fixup: count = %d", count);
+    DBG(" %d entries", count);
+
+    return changed;
 }
 
-static inline void _yed_syntax_cache_rebuild(yed_syntax *syntax, yed_buffer *buffer, int row, int mod_event) {
-    _yed_syntax_cache       *cache;
+static inline void _yed_syntax_cache_rebuild(yed_syntax *syntax, _yed_syntax_cache *cache, yed_buffer *buffer, int row, int mod_event) {
     yed_line                *line;
     _yed_syntax_range       *cached_state;
     _yed_syntax_range       *start_state;
     _yed_syntax_range       *end_state;
+    int                      mark_dirty;
     _yed_syntax_cache_entry *it;
     int                      idx;
 
-    if (!syntax->finalized || buffer == NULL) { return; }
+    if (!syntax->finalized) { return; }
 
-    cache = _yed_syntax_get_cache(syntax, buffer);
+    mark_dirty = 0;
 
     switch (mod_event) {
         case BUFF_MOD_APPEND_TO_LINE:
@@ -708,12 +797,7 @@ static inline void _yed_syntax_cache_rebuild(yed_syntax *syntax, yed_buffer *buf
             end_state   = _yed_syntax_get_line_end_state(syntax, buffer, line, start_state);
 
             _yed_syntax_add_to_cache(syntax, cache, row + 1, end_state);
-            if (cached_state != end_state) {
-                _yed_syntax_fixup_cache(syntax, buffer, cache, row + 1);
-                if (!end_state->one_line) {
-                    yed_mark_dirty_frames(buffer);
-                }
-            }
+            mark_dirty = _yed_syntax_fixup_cache(syntax, buffer, cache, row + 1);
 
             break;
 
@@ -731,7 +815,7 @@ static inline void _yed_syntax_cache_rebuild(yed_syntax *syntax, yed_buffer *buf
 
             cached_state = _yed_syntax_get_start_state(syntax, buffer, row);
             _yed_syntax_add_to_cache(syntax, cache, row, cached_state);
-            _yed_syntax_fixup_cache(syntax, buffer, cache, row + 1);
+            mark_dirty = _yed_syntax_fixup_cache(syntax, buffer, cache, row + 1);
 
             break;
 
@@ -750,16 +834,23 @@ static inline void _yed_syntax_cache_rebuild(yed_syntax *syntax, yed_buffer *buf
             }
 
 
-            start_state = _yed_syntax_get_start_state(syntax, buffer, row);
-            _yed_syntax_add_to_cache(syntax, cache, row, start_state);
-            _yed_syntax_fixup_cache(syntax, buffer, cache, row - 1);
+/*             start_state = _yed_syntax_get_start_state(syntax, buffer, row); */
+/*             _yed_syntax_add_to_cache(syntax, cache, row, start_state); */
+            if (row == 1) {
+                _yed_syntax_add_to_cache(syntax, cache, row, syntax->global);
+            }
+            mark_dirty = _yed_syntax_fixup_cache(syntax, buffer, cache, row - 1);
 
             break;
 
         case BUFF_MOD_CLEAR:
             _yed_syntax_build_cache(syntax, buffer);
-            yed_mark_dirty_frames(buffer);
+            mark_dirty = 1;
             break;
+    }
+
+    if (mark_dirty) {
+        yed_mark_dirty_frames(buffer);
     }
 }
 
@@ -785,7 +876,7 @@ static inline const char * _yed_syntax_find_next_kwd(yed_syntax *syntax, _yed_sy
         len = yed_get_glyph_len(*g);
         if (len > 1) { goto next; }
 
-        if (isalpha(g->c) || g->c == '_') {
+        if (is_alpha(g->c) || g->c == '_') {
             col      = yed_line_idx_to_col(line, ((void*)g) - ((void*)array_data(line->chars)));
             word_len = _yed_syntax_line_get_word_len(line, col);
             word     = &g->c;
@@ -935,13 +1026,20 @@ static inline const char * _yed_syntax_find_range_end(yed_syntax *syntax, _yed_s
                     }
                 }
 
-                *len_out    = m.rm_eo - m.rm_so;
-                match_start = start + m.rm_so;
+                *len_out = m.rm_eo - m.rm_so;
+                return start + m.rm_so;
             }
         }
 
-        return match_start;
+        goto out;
 next:;
+    }
+
+out:;
+    *len_out = 0;
+
+    if (range->one_line) {
+        return end;
     }
 
     return NULL;
@@ -1241,9 +1339,9 @@ static inline void yed_syntax_attr_pop(yed_syntax *syntax) {
 }
 
 static inline int _yed_syntax_add_regex(yed_syntax *syntax, const char *pattern, int group) {
-    int            err;
+    int                err;
     _yed_syntax_regex  r;
-    size_t         err_len;
+    size_t             err_len;
     _yed_syntax_range *range;
 
     memset(&r, 0, sizeof(r));
@@ -1280,8 +1378,8 @@ static inline int yed_syntax_regex_sub(yed_syntax *syntax, const char *pattern, 
 
 static inline int yed_syntax_range_start(yed_syntax *syntax, const char *pattern) {
     _yed_syntax_range *range;
-    int            err;
-    size_t         err_len;
+    int                err;
+    size_t             err_len;
 
     if (_yed_syntax_top_range(syntax) != syntax->global) { return -1; }
 
@@ -1308,8 +1406,8 @@ static inline int yed_syntax_range_start(yed_syntax *syntax, const char *pattern
 
 static inline int yed_syntax_range_end(yed_syntax *syntax, const char *pattern) {
     _yed_syntax_range *range;
-    int            err;
-    size_t         err_len;
+    int                err;
+    size_t             err_len;
 
     range = _yed_syntax_top_range(syntax);
     if (range == syntax->global) { return -1; }
@@ -1323,7 +1421,6 @@ static inline int yed_syntax_range_end(yed_syntax *syntax, const char *pattern) 
         regerror(err, &range->end, syntax->regex_err_str, err_len);
         _yed_syntax_free_range(range);
     } else {
-        array_push(syntax->ranges, range);
         syntax->range = NULL;
     }
 
@@ -1332,9 +1429,9 @@ static inline int yed_syntax_range_end(yed_syntax *syntax, const char *pattern) 
 
 static inline int yed_syntax_range_skip(yed_syntax *syntax, const char *pattern) {
     _yed_syntax_range *range;
-    int            err;
-    regex_t        reg;
-    size_t         err_len;
+    int                err;
+    regex_t            reg;
+    size_t             err_len;
 
     range = _yed_syntax_top_range(syntax);
     if (range == syntax->global) { return -1; }
@@ -1408,11 +1505,23 @@ static inline void yed_syntax_style_event(yed_syntax *syntax, yed_event *event) 
 }
 
 static inline void yed_syntax_buffer_delete_event(yed_syntax *syntax, yed_event *event) {
-    _yed_syntax_remove_cache(syntax, event->buffer);
+    CACHE_IT it;
+
+    it = tree_lookup(syntax->caches, event->buffer);
+
+    if (tree_it_good(it)) {
+        _yed_syntax_remove_cache(syntax, event->buffer);
+    }
 }
 
 static inline void yed_syntax_buffer_mod_event(yed_syntax *syntax, yed_event *event) {
-    _yed_syntax_cache_rebuild(syntax, event->buffer, event->row, event->buff_mod_event);
+    CACHE_IT it;
+
+    it = tree_lookup(syntax->caches, event->buffer);
+
+    if (tree_it_good(it)) {
+        _yed_syntax_cache_rebuild(syntax, &tree_it_val(it), event->buffer, event->row, event->buff_mod_event);
+    }
 }
 
 
